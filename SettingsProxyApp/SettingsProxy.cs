603d3f1381c00110963da1ae32e73eff269d3654
Eric Andres <eric-andres@emmersionlearning.com>
2020-06-03 17:08:14 -0400

    Add in configuration root stuff

--- SettingsProxyApp/SettingsProxy.cs
+++ SettingsProxyApp/SettingsProxy.cs
@@ -1,54 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
 
 namespace SettingsProxyApp
 {
     public class SettingsProxy: DispatchProxy
     {
-        private Dictionary<string, string> settingNames;
+        private Dictionary<string, SettingInfo> settingInfos;
+        private IConfigurationRoot configurationRoot;
 
-        public static TInterface Create<TInterface>(Dictionary<string, string> settingNames)
+        private class SettingInfo
+        {
+            public string Name { get; set; }
+            public Type Type { get; set; }
+        }
+
+        public static TInterface Create<TInterface>(Dictionary<string, string> settingNames, IConfigurationRoot configurationRoot)
         {
             var result = DispatchProxy.Create<TInterface, SettingsProxy>();
             if (result is SettingsProxy proxy)
             {
                 Console.WriteLine("setting the dictionary");
-                proxy.settingNames = settingNames;
+                proxy.OverrideSettingNames(settingNames);
+                proxy.configurationRoot = configurationRoot;
             }
+            
             return result;
         }
 
         protected override object Invoke(MethodInfo targetMethod, object[] args)
         {
             EnsureSettingNameCache(targetMethod.DeclaringType);
-            return settingNames[targetMethod.Name];
+            var settingInfo = settingInfos[targetMethod.Name];
+            return configurationRoot.GetValue(settingInfo.Type, settingInfo.Name);
+        }
+
+        private void OverrideSettingNames(Dictionary<string, string> settingNames)
+        {
+            settingInfos = settingNames.ToDictionary(x => x.Key, x => new SettingInfo {Name = x.Value});
         }
 
         private void EnsureSettingNameCache(Type type)
         {
-            if (settingNames == null)
+            if (settingInfos == null)
             {
-                settingNames = new Dictionary<string, string>();
+                settingInfos = new Dictionary<string, SettingInfo>();
             }
 
             var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
             foreach (var property in properties)
             {
-                var getterName = property.GetGetMethod(false).Name;
-                if (settingNames.ContainsKey(getterName))
+                var getter = property.GetGetMethod(false);
+                var getterName = getter.Name;
+                if (!settingInfos.ContainsKey(getterName))
                 {
-                    continue;
+                    settingInfos[getterName] = new SettingInfo();
                 }
 
+                settingInfos[getterName].Type = getter.ReturnType;
                 if (property.GetCustomAttribute<SettingNameAttribute>() is SettingNameAttribute attr)
                 {
-                    settingNames[getterName] = attr.Name;
+                    settingInfos[getterName].Name = attr.Name;
                 }
                 else
                 {
-                    settingNames[getterName] = Regex.Replace(getterName, "^get_", "");
+                    settingInfos[getterName].Name = Regex.Replace(getterName, "^get_", "");
                 }
             }
         }
